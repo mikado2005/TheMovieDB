f4ced6dd7ea96df3041b7a41027760eac70348f8
Greg Anderson <greg@planetbeagle.com>
2018-03-15 21:08:27 -0400

    Starting to build out MovieListVC

--- TheMovieDB/MovieListVC.swift
+++ TheMovieDB/MovieListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MovieListVC.swift
 //  TheMovieDB
 //
 //  Created by Greg Anderson on 3/15/18.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MovieListVC: UIViewController,
+                   UITabBarDelegate,
+                   UITableViewDelegate,
+                   UITableViewDataSource, {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
