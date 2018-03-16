308bca836f76452b4ef506e6b7659e5c611e3c5c
Greg Anderson <greg@planetbeagle.com>
2018-03-15 20:33:54 -0400

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Greg Anderson on 3/15/18.
+//  Copyright © 2018 Planet Beagle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
